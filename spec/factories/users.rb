ebac5effe8bfd9861af7ad44357be5251b669e81
Pete Broderick <simplybinary@gmail.com>
2012-03-29 23:59:09 -0400

    adding rspec stuff

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,15 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequency :user_email do
+    "user#{SecureRandom.hex(10)}@example.com"
+  end
+  sequency :username do
+    "user#{SecureRandom.hex(5)}"
+  end
   factory :user do
+    u.username { FactoryGirl.generate :username }
+    u.email { FactoryGirl.generate :user_email }
+    u.password 'password123'
   end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  # Other things
+
+  # Clean up the database
+  require 'database_cleaner'
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = "mongoid"
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.clean
+  end
+end
